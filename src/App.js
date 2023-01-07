5fcdcabb961c465bc5ca452415ea5c6bd7159861
Akash Suklabaidya <akashsuklabaidya593@gmail.com>
2023-01-07 14:17:14 +0530

    Commited

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Buttons from "./components/Buttons"
-import Dark from "./components/Dark"
+
 
 function App() {
 
-  const [darkTheme, setDarkTheme] = useState(false);
 
   return (
-    <div className={darkTheme ? 'dark' : ''} >
-      <div className="bg-stone-200 dark:bg-gray-900">
-        <Dark darkTheme={darkTheme} setDarkTheme={setDarkTheme} />
+    <div  >
+      <div className="bg-stone-200 ">
         <Buttons />
       </div>
     </div>
